543028eb8ae4ea118b93c23a57f11581cc0ed6fd
Mitokongdrya <josiahliebert@gmail.com>
2025-10-12 12:55:41 -0700

    Add main image for each state

--- data/states.ts
+++ data/states.ts
@@ -3,6 +3,7 @@ export const states = [
   {
     code: "CA",
     name: "California",
+    image: "/images/CA.png",
     plates: [
       { id: 1, name: "Standard", image: "/plates/CA.png" },
       { id: 2, name: "Legacy", image: "/plates/CA-legacy.png" },
@@ -12,6 +13,7 @@ export const states = [
   {
     code: "OR",
     name: "Oregon",
+    image: "/images/OR.jpg",
     plates: [
       { id: 1, name: "Standard", image: "/plates/OR.jpg" },
       { id: 2, name: "Crater Lake", image: "/plates/OR-crater.png" },
