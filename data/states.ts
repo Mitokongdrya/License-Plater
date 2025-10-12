17cfea1a1a177c226d3eec0ae4f2868a0848e934
Mitokongdrya <josiahliebert@gmail.com>
2025-10-12 13:27:56 -0700

    add all states to data

--- data/states.ts
+++ data/states.ts
@@ -1,25 +1,456 @@
 // data/states.ts
 export const states = [
-  {
-    code: "CA",
-    name: "California",
-    image: "/images/CA.png",
-    plates: [
-      { id: 1, name: "Standard", image: "/plates/CA.png" },
-      { id: 2, name: "Legacy", image: "/plates/CA-legacy.png" },
-      { id: 3, name: "Yosemite", image: "/plates/CA-yosemite.png" },
-    ],
-  },
-  {
-    code: "OR",
-    name: "Oregon",
-    image: "/images/OR.jpg",
-    plates: [
-      { id: 1, name: "Standard", image: "/plates/OR.jpg" },
-      { id: 2, name: "Crater Lake", image: "/plates/OR-crater.png" },
-    ],
-  },
-  // ...more states
+    {
+        code: "AL",
+        name: "Alabama",
+        image: "/images/AL.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/AL.png" },
+            { id: 2, name: "Sweet Home Alabama", image: "/plates/AL-sweethome.png" },
+        ],
+    },
+    {
+        code: "AK",
+        name: "Alaska",
+        image: "/images/AK.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/AK.png" },
+            { id: 2, name: "Bear", image: "/plates/AK-bear.png" },
+        ],
+    },
+    {
+        code: "AZ",
+        name: "Arizona",
+        image: "/images/AZ.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/AZ.png" },
+            { id: 2, name: "Grand Canyon", image: "/plates/AZ-grandcanyon.png" },
+        ],
+    },
+    {
+        code: "AR",
+        name: "Arkansas",
+        image: "/images/AR.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/AR.png" },
+            { id: 2, name: "Diamond", image: "/plates/AR-diamond.png" },
+        ],
+    },
+    {
+        code: "CA",
+        name: "California",
+        image: "/images/CA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/CA.png" },
+            { id: 2, name: "Legacy", image: "/plates/CA-legacy.png" },
+            { id: 3, name: "Yosemite", image: "/plates/CA-yosemite.png" },
+        ],
+    },
+    {
+        code: "CO",
+        name: "Colorado",
+        image: "/images/CO.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/CO.png" },
+            { id: 2, name: "Mountains", image: "/plates/CO-mountains.png" },
+        ],
+    },
+    {
+        code: "CT",
+        name: "Connecticut",
+        image: "/images/CT.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/CT.png" },
+            { id: 2, name: "Constitution State", image: "/plates/CT-constitution.png" },
+        ],
+    },
+    {
+        code: "DE",
+        name: "Delaware",
+        image: "/images/DE.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/DE.png" },
+            { id: 2, name: "First State", image: "/plates/DE-firststate.png" },
+        ],
+    },
+    {
+        code: "FL",
+        name: "Florida",
+        image: "/images/FL.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/FL.png" },
+            { id: 2, name: "Orange", image: "/plates/FL-orange.png" },
+        ],
+    },
+    {
+        code: "GA",
+        name: "Georgia",
+        image: "/images/GA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/GA.png" },
+            { id: 2, name: "Peach State", image: "/plates/GA-peach.png" },
+        ],
+    },
+    {
+        code: "HI",
+        name: "Hawaii",
+        image: "/images/HI.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/HI.png" },
+            { id: 2, name: "Rainbow", image: "/plates/HI-rainbow.png" },
+        ],
+    },
+    {
+        code: "ID",
+        name: "Idaho",
+        image: "/images/ID.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/ID.png" },
+            { id: 2, name: "Famous Potatoes", image: "/plates/ID-potatoes.png" },
+        ],
+    },
+    {
+        code: "IL",
+        name: "Illinois",
+        image: "/images/IL.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/IL.png" },
+            { id: 2, name: "Land of Lincoln", image: "/plates/IL-lincoln.png" },
+        ],
+    },
+    {
+        code: "IN",
+        name: "Indiana",
+        image: "/images/IN.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/IN.png" },
+            { id: 2, name: "Crossroads of America", image: "/plates/IN-crossroads.png" },
+        ],
+    },
+    {
+        code: "IA",
+        name: "Iowa",
+        image: "/images/IA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/IA.png" },
+            { id: 2, name: "County", image: "/plates/IA-county.png" },
+        ],
+    },
+    {
+        code: "KS",
+        name: "Kansas",
+        image: "/images/KS.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/KS.png" },
+            { id: 2, name: "Sunflower State", image: "/plates/KS-sunflower.png" },
+        ],
+    },
+    {
+        code: "KY",
+        name: "Kentucky",
+        image: "/images/KY.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/KY.png" },
+            { id: 2, name: "Bluegrass State", image: "/plates/KY-bluegrass.png" },
+        ],
+    },
+    {
+        code: "LA",
+        name: "Louisiana",
+        image: "/images/LA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/LA.png" },
+            { id: 2, name: "Pelican", image: "/plates/LA-pelican.png" },
+        ],
+    },
+    {
+        code: "ME",
+        name: "Maine",
+        image: "/images/ME.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/ME.png" },
+            { id: 2, name: "Lobster", image: "/plates/ME-lobster.png" },
+        ],
+    },
+    {
+        code: "MD",
+        name: "Maryland",
+        image: "/images/MD.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MD.png" },
+            { id: 2, name: "Chesapeake Bay", image: "/plates/MD-chesapeake.png" },
+        ],
+    },
+    {
+        code: "MA",
+        name: "Massachusetts",
+        image: "/images/MA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MA.png" },
+            { id: 2, name: "Spirit of America", image: "/plates/MA-spirit.png" },
+        ],
+    },
+    {
+        code: "MI",
+        name: "Michigan",
+        image: "/images/MI.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MI.png" },
+            { id: 2, name: "Great Lakes", image: "/plates/MI-greatlakes.png" },
+        ],
+    },
+    {
+        code: "MN",
+        name: "Minnesota",
+        image: "/images/MN.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MN.png" },
+            { id: 2, name: "10,000 Lakes", image: "/plates/MN-lakes.png" },
+        ],
+    },
+    {
+        code: "MS",
+        name: "Mississippi",
+        image: "/images/MS.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MS.png" },
+            { id: 2, name: "Magnolia State", image: "/plates/MS-magnolia.png" },
+        ],
+    },
+    {
+        code: "MO",
+        name: "Missouri",
+        image: "/images/MO.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MO.png" },
+            { id: 2, name: "Show-Me State", image: "/plates/MO-showme.png" },
+        ],
+    },
+    {
+        code: "MT",
+        name: "Montana",
+        image: "/images/MT.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/MT.png" },
+            { id: 2, name: "Big Sky Country", image: "/plates/MT-bigsky.png" },
+        ],
+    },
+    {
+        code: "NE",
+        name: "Nebraska",
+        image: "/images/NE.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NE.png" },
+            { id: 2, name: "Sandhills", image: "/plates/NE-sandhills.png" },
+        ],
+    },
+    {
+        code: "NV",
+        name: "Nevada",
+        image: "/images/NV.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NV.png" },
+            { id: 2, name: "Lake Tahoe", image: "/plates/NV-tahoe.png" },
+        ],
+    },
+    {
+        code: "NH",
+        name: "New Hampshire",
+        image: "/images/NH.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NH.png" },
+            { id: 2, name: "Old Man of the Mountain", image: "/plates/NH-oldman.png" },
+        ],
+    },
+    {
+        code: "NJ",
+        name: "New Jersey",
+        image: "/images/NJ.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NJ.png" },
+            { id: 2, name: "Garden State", image: "/plates/NJ-garden.png" },
+        ],
+    },
+    {
+        code: "NM",
+        name: "New Mexico",
+        image: "/images/NM.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NM.png" },
+            { id: 2, name: "Chile", image: "/plates/NM-chile.png" },
+        ],
+    },
+    {
+        code: "NY",
+        name: "New York",
+        image: "/images/NY.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NY.png" },
+            { id: 2, name: "Empire Gold", image: "/plates/NY-empire.png" },
+        ],
+    },
+    {
+        code: "NC",
+        name: "North Carolina",
+        image: "/images/NC.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/NC.png" },
+            { id: 2, name: "First in Flight", image: "/plates/NC-flight.png" },
+        ],
+    },
+    {
+        code: "ND",
+        name: "North Dakota",
+        image: "/images/ND.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/ND.png" },
+            { id: 2, name: "Peace Garden State", image: "/plates/ND-peacegarden.png" },
+        ],
+    },
+    {
+        code: "OH",
+        name: "Ohio",
+        image: "/images/OH.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/OH.png" },
+            { id: 2, name: "Birthplace of Aviation", image: "/plates/OH-aviation.png" },
+        ],
+    },
+    {
+        code: "OK",
+        name: "Oklahoma",
+        image: "/images/OK.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/OK.png" },
+            { id: 2, name: "Scissortail", image: "/plates/OK-scissortail.png" },
+        ],
+    },
+    {
+        code: "OR",
+        name: "Oregon",
+        image: "/images/OR.jpg",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/OR.jpg" },
+            { id: 2, name: "Crater Lake", image: "/plates/OR-crater.png" },
+        ],
+    },
+    {
+        code: "PA",
+        name: "Pennsylvania",
+        image: "/images/PA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/PA.png" },
+            { id: 2, name: "Keystone State", image: "/plates/PA-keystone.png" },
+        ],
+    },
+    {
+        code: "RI",
+        name: "Rhode Island",
+        image: "/images/RI.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/RI.png" },
+            { id: 2, name: "Ocean State", image: "/plates/RI-ocean.png" },
+        ],
+    },
+    {
+        code: "SC",
+        name: "South Carolina",
+        image: "/images/SC.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/SC.png" },
+            { id: 2, name: "Palmetto State", image: "/plates/SC-palmetto.png" },
+        ],
+    },
+    {
+        code: "SD",
+        name: "South Dakota",
+        image: "/images/SD.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/SD.png" },
+            { id: 2, name: "Mount Rushmore", image: "/plates/SD-rushmore.png" },
+        ],
+    },
+    {
+        code: "TN",
+        name: "Tennessee",
+        image: "/images/TN.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/TN.png" },
+            { id: 2, name: "Volunteer State", image: "/plates/TN-volunteer.png" },
+        ],
+    },
+    {
+        code: "TX",
+        name: "Texas",
+        image: "/images/TX.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/TX.png" },
+            { id: 2, name: "Lone Star", image: "/plates/TX-lonestar.png" },
+        ],
+    },
+    {
+        code: "UT",
+        name: "Utah",
+        image: "/images/UT.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/UT.png" },
+            { id: 2, name: "Arches", image: "/plates/UT-arches.png" },
+        ],
+    },
+    {
+        code: "VT",
+        name: "Vermont",
+        image: "/images/VT.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/VT.png" },
+            { id: 2, name: "Green Mountain State", image: "/plates/VT-greenmountain.png" },
+        ],
+    },
+    {
+        code: "VA",
+        name: "Virginia",
+        image: "/images/VA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/VA.png" },
+            { id: 2, name: "Virginia is for Lovers", image: "/plates/VA-lovers.png" },
+        ],
+    },
+    {
+        code: "WA",
+        name: "Washington",
+        image: "/images/WA.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/WA.png" },
+            { id: 2, name: "Mount Rainier", image: "/plates/WA-rainier.png" },
+        ],
+    },
+    {
+        code: "WV",
+        name: "West Virginia",
+        image: "/images/WV.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/WV.png" },
+            { id: 2, name: "Mountain State", image: "/plates/WV-mountain.png" },
+        ],
+    },
+    {
+        code: "WI",
+        name: "Wisconsin",
+        image: "/images/WI.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/WI.png" },
+            { id: 2, name: "America's Dairyland", image: "/plates/WI-dairyland.png" },
+        ],
+    },
+    {
+        code: "WY",
+        name: "Wyoming",
+        image: "/images/WY.png",
+        plates: [
+            { id: 1, name: "Standard", image: "/plates/WY.png" },
+            { id: 2, name: "Buckin' Horse", image: "/plates/WY-buckin.png" },
+        ],
+    },
 ];
 
 
