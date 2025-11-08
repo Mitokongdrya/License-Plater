10aa2c5ba4987f2168f9f75300d9a55668d8e096
Mitokongdrya <josiahliebert@gmail.com>
2025-11-07 16:28:53 -0800

    Write app intro and current dev steps

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,31 @@ import Navbar from "../components/layout/Navbar";
 export default function Home() {
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-bold">Welcome to Plate Finder</h1>
-      <p className="mt-2 text-gray-600">Track and explore license plates by state!</p>
+      <h1 className="text-5xl md:text-6xl font-bold text-center">Welcome to License Plater</h1>
+      <p className="mt-2">
+        When I was a child, my grandma gave me this flipboard game that encouraged me to record all of the different license plates I saw.
+        I completed the entire US in less than six months, and it was a blast!
+        This sparked a lifelong interest in license plates, and geography as a whole.
+      </p>
+      <p className="mt-4">
+        Summer 2025, I found the original flipboard, and used it again to track plates.
+        It was awesome, but it had some limitations including a cumbersome size and limited plate designs.
+        The goal of this application is to help users track the United States license plates that they encounter.
+      </p>
+      <div className="mt-6">
+        <p className="mb-4">
+          This app is currently in active development. 
+          If you'd like to use it and give feedback, please feel free to reach out!
+          The current next steps include:
+        </p>
+        <ul className="list-disc list-inside">
+          <li>Implement user authentication and profiles through a dataase</li>
+          <li>Build out the plate matching form</li>
+          <li>Add functionality to the map based on Plate Index data</li>
+          <li>Implement a user-friendly dashboard for tracking plates</li>
+          <li>Implement some head-to-head competition features</li>
+        </ul>
+      </div>
     </main>
   );
 }
-
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body className="min-h-screen flex flex-col">
-//         <Navbar />  {/* 👈 appears at top of every page */}
-//         <main className="flex-1 p-6">{children}</main>
-//         <footer className="p-4 text-center text-sm text-gray-500">
-//           © {new Date().getFullYear()} My App
-//         </footer>
-//       </body>
-//     </html>
-//   );
-// }
